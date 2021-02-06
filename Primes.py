fead11f79e3e66f9ee39ba6d1a429b244fd8224d
RoBoLilly <ultrahoodsense@gmail.com>
2021-02-06 16:34:30 -0700

    Euclidean Algorithm and timing tests.

--- Primes.py
+++ Primes.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import time
 
-def factors(n):
+def factorsSlow(n):
     f = []
     for d in list(range(1, n+1)) :
         if not (round(n/d)-(n/d)) :
+            f.append(d)
+    return f
+
+def factors(n) :
+    f = []
+    for d in list(range(1, n+1)) :
+        if not n % d : # because duh
             f.append(d)
     return f
 
@@ -15,7 +23,7 @@ def isPrime(n):
 def primes(n):
     f = []
     for d in list(range(1, n)) :
-        if isPrime(n) :
+        if isPrime(d) :
             f.append(d)
     return f
 
@@ -38,11 +46,18 @@ def commonPrimeFactors(n, m):
                 l.append(f)
     return l
 
-def GCD(x, y) :
+def GCD_slow(x, y) :
     return max(commonPrimeFactors(x, y) if len(commonPrimeFactors(x, y)) != 0 else [0])
 
-# Examples
+def GCD(x, y) : # using Euclidean Algorithm
+    for l in range(0, x):
+        if (x - y) == 0 :
+            return o
+        o = abs(x - y)
+        x = min([x, y])
+        y = o
 
+# Examples
 
 print("factors(6) = ", factors(6))
 print("factors(64) = " , factors(64))
@@ -66,7 +81,41 @@ print("GCD(2021, 4465) = ", GCD(2021, 4465))
 print("GCD(9, 15) = ", GCD(9, 15))
 print("GCD(10, 15) = ", GCD(1, 15))
 
+print("\nSpeed Tests: \n")
 
+timerStart = time.perf_counter()
+print("GCD_slow(2021, 4465) = ", GCD_slow(2021, 4465))
+timerEnd = time.perf_counter()
+print(f"Time in {timerEnd - timerStart:0.7f}(s)")
+
+timerStart = time.perf_counter()
+print("GCD(2021, 4465) = ", GCD(2021, 4465))
+timerEnd = time.perf_counter()
+print(f"Time in {timerEnd - timerStart:0.7f}(s)")
+
+print("\n")
+
+timerStart = time.perf_counter()
+print("GCD_slow(9, 15) = ", GCD_slow(9, 15))
+timerEnd = time.perf_counter()
+print(f"Time in {timerEnd - timerStart:0.7f}(s)")
+
+timerStart = time.perf_counter()
+print("GCD(9, 15) = ", GCD(9, 15))
+timerEnd = time.perf_counter()
+print(f"Time in {timerEnd - timerStart:0.7f}(s)")
+
+print("\n")
+
+timerStart = time.perf_counter()
+print("GCD_slow(23423, 53456) = ", GCD_slow(23423, 53456))
+timerEnd = time.perf_counter()
+print(f"Time in {timerEnd - timerStart:0.7f}(s)")
+
+timerStart = time.perf_counter()
+print("GCD(23423, 53456) = ", GCD(23423, 53456))
+timerEnd = time.perf_counter()
+print(f"Time in {timerEnd - timerStart:0.7f}(s)")
 
             
 
